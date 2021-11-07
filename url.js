052f7c6b0431ada77dcbd2a9b0735dc222a1aeee
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-11-07 02:15:29 +0100

    FEAT: add custom charts urls

--- url.js
+++ url.js
@@ -1,4 +1,9 @@
-const { CARD_SCREENS, MULTICARD_SCREENS, CHART_SCREENS } = require("./screens");
+const {
+  CARD_SCREENS,
+  MULTICARD_SCREENS,
+  CHART_SCREENS,
+  CUSTOM_CHART_SCREENS,
+} = require("./screensDict");
 
 const AWS_URL =
   "https://325sfff4r2.execute-api.eu-central-1.amazonaws.com/sledilnikScreenshot";
@@ -19,5 +24,12 @@ exports.MULTICARD_URL = MULTICARD_SCREENS.reduce((acc, screen) => {
 
 exports.CHART_URL = CHART_SCREENS.reduce((acc, screen) => {
   acc[screen] = `${CHART_URL}&screen=${screen}`;
+  return acc;
+}, {});
+
+exports.CUSTOM_CHARTS_QUERY_PARAMS = Object.entries(
+  CUSTOM_CHART_SCREENS
+).reduce((acc, [key, { screen, custom }]) => {
+  acc[key] = `${CHART_URL}&screen=${screen}&custom=${custom}`;
   return acc;
 }, {});
